72447868dd813d088e52248b9f64681e5d7f500f
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-07-06 00:44:47 +0530

    feat: integrate profile module into store and api services and add software blog data file

--- src/api/index.js
+++ src/api/index.js
@@ -5,34 +5,17 @@
 
 import axiosInstance from './axiosInstance';
 import authAPI from './authAPI';
+import profileAPI from './profileAPI';
 import robotAPI from './robotAPI';
 import smartHomeAPI from './smartHomeAPI';
 import monitoringAPI from './monitoringAPI';
 
-/**
- * Example usage:
- * import { authAPI, robotAPI, smartHomeAPI, monitoringAPI } from '@/api'
- * 
- * // Authentication
- * await authAPI.login(email, password)
- * 
- * // Robot Control
- * await robotAPI.control(robotId, 'move', { direction: 'forward', speed: 50 })
- * 
- * // Smart Home
- * await smartHomeAPI.getDevices()
- * await smartHomeAPI.triggerScene(sceneId)
- * 
- * // Monitoring
- * await monitoringAPI.getTelemetry({ limit: 100 })
- */
+export { axiosInstance, authAPI, profileAPI, robotAPI, smartHomeAPI, monitoringAPI };
 
-export { axiosInstance, authAPI, robotAPI, smartHomeAPI, monitoringAPI };
-
-// Default export for convenience
 export default {
   axios: axiosInstance,
   auth: authAPI,
+  profile: profileAPI,
   robot: robotAPI,
   smartHome: smartHomeAPI,
   monitoring: monitoringAPI,
